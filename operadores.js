79be56268cb47a96564aa4ab9715947395b06cd4
Diego <dileo312@hotmail.comclear>
2024-01-24 19:19:25 -0600

    Operadores

---
+++ operadores.js
@@ -0,0 +1,38 @@
+/* Operadores de incremento y decremento */
+
+let iterador = 0;
+
+console.log(iterador);
+
+iterador++;
+
+console.log(iterador);
+iterador--;
+console.log(iterador)
+iterador += 2;
+console.log(iterador)
+iterador += 2;
+console.log(iterador)
+
+/* Operadores de comparacion */
+
+console.log(2 > 9);
+console.log(2 < 9);
+console.log(4 <= 4);
+console.log(4 > 4);
+console.log(3 != 4);
+
+// == ===
+
+let variable_1 = 5;
+let variable_2 = 10;
+let variable_3 = '5';
+
+/* console.log();
+console.log();
+console.log(); */
+
+// Operadores logicos
+
+console.log(true && false)
+console.log(true || false)
